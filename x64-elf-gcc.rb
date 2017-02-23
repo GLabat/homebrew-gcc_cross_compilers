20bd52c9dd517796dfc9c473a549a8f9ffab280e
GLabat <labat.guillaume@gmail.com>
2017-02-23 14:15:02 +0100

    Real sha256 + gcc4.8

--- x64-elf-gcc.rb
+++ x64-elf-gcc.rb
@@ -4,7 +4,7 @@ class X64ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  sha256 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
+  sha256 'b037fe5132b71ecad2ea7141ec92292b5d32427bf90fd90cde432b1d5abacc2c'
 
   depends_on 'gmp'
   depends_on 'libmpc'
@@ -14,10 +14,10 @@ class X64ElfGcc < Formula
   def install
     binutils = Formula.factory 'x64-elf-binutils'
 
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['CC'] = '/usr/local/bin/gcc-4.8'
+    ENV['CXX'] = '/usr/local/bin/g++-4.8'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.8'
+    ENV['LD'] = '/usr/local/bin/gcc-4.8'
     ENV['PATH'] += ":#{binutils.prefix/"bin"}"
 
     mkdir 'build' do
